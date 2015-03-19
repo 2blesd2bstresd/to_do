1aa5496735d5c02bff5fece689aa2d7fb6a9c3fd
Maxwell Howard <maxh@spotkey.com>
2015-03-19 16:43:16 -0700

    fix

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def add_user(name):
     print "IS IT CLOSED: ", conn.closed
     print "CURSOR: ", dir(c)
     try:
-        c.execute("INSERT INTO users (first_name) VALUES (%s)" % str(name))
+        c.execute('INSERT INTO users (first_name) VALUES (%s)' % str(name))
     except psycopg2.Error as e:
         print 'HERES THE ERROR: ', e.diag.message_primary
         return 'nooooo'
