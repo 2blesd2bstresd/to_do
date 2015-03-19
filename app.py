576378c71c2b7b0101e31cf960e637e09a3192a5
Maxwell Howard <maxh@spotkey.com>
2015-03-19 13:58:20 -0700

    fix

--- app.py
+++ app.py
@@ -22,79 +22,79 @@ def get_conn_cursor():
 
 app = Flask(__name__)
 
-# users = [
-#     {
-#         'id': 1,
-#         'name': u'Max',
-#         'profile_url': u'http://bit.ly/1xz3ZlP',
-#         'spotkeys': [{'name': u'home', 'id': 1},
-#                     {'name': u'work', 'id': 5}],
-#         'contacts': [{'name': u'Angelica', 'id': 2,},
-#                      {'name': u'Tim', 'id': 3}] 
-#     },
-#     {
-#         'id': 2,
-#         'title': u'Angelica',
-#         'profile_url': u'http://bit.ly/1xz3ZlP',
-#         'spotkeys': [{'name': u'home', 'id': 2}],
-#         'contacts': [{'name': u'Max', 'id': 1,},
-#                      {'name': u'Tim', 'id': 3}] 
-#     },
-#     {
-#         'id': 3,
-#         'name': u'Tim',
-#         'profile_url': u'http://bit.ly/1xz3ZlP',
-#         'spotkeys': [{'name': u'home', 'id': 3}],
-#         'contacts': [{'name': u'Angelica', 'id': 2,},
-#                      {'name': u'Max', 'id': 1}] 
-#     },
-#     {
-#         'id': 4,
-#         'title': u'Mike',
-#         'profile_url': u'http://bit.ly/1xz3ZlP',
-#         'spotkeys': [{'name': u'home', 'id': 4}],
-#         'contacts' : [] 
-#     },
+users = [
+    {
+        'id': 1,
+        'name': u'Max',
+        'profile_url': u'http://bit.ly/1xz3ZlP',
+        'spotkeys': [{'name': u'home', 'id': 1},
+                    {'name': u'work', 'id': 5}],
+        'contacts': [{'name': u'Angelica', 'id': 2,},
+                     {'name': u'Tim', 'id': 3}] 
+    },
+    {
+        'id': 2,
+        'title': u'Angelica',
+        'profile_url': u'http://bit.ly/1xz3ZlP',
+        'spotkeys': [{'name': u'home', 'id': 2}],
+        'contacts': [{'name': u'Max', 'id': 1,},
+                     {'name': u'Tim', 'id': 3}] 
+    },
+    {
+        'id': 3,
+        'name': u'Tim',
+        'profile_url': u'http://bit.ly/1xz3ZlP',
+        'spotkeys': [{'name': u'home', 'id': 3}],
+        'contacts': [{'name': u'Angelica', 'id': 2,},
+                     {'name': u'Max', 'id': 1}] 
+    },
+    {
+        'id': 4,
+        'title': u'Mike',
+        'profile_url': u'http://bit.ly/1xz3ZlP',
+        'spotkeys': [{'name': u'home', 'id': 4}],
+        'contacts' : [] 
+    },
 
 
-# ]
+]
 
-# spotkeys = [
-#     {
-#         'title': 'home',
-#         'id': 1,
-#         'owner_id':1,
-#         'longitude': 37.7813967,
-#         'latitude': -122.4044686
-#     },
-#     {
-#         'title': 'home',
-#         'id': 2,
-#         'owner_id':2,
-#         'longitude': 37.7813967,
-#         'latitude': -122.4044686
-#     },    
-#     {
-#         'title': 'home',
-#         'id': 3,
-#         'owner_id':3,
-#         'longitude': 37.7813967,
-#         'latitude': -122.4044686
-#     },
-#     {
-#         'title': 'home',
-#         'id': 4,
-#         'owner_id':4,
-#         'longitude': 37.7813967,
-#         'latitude': -122.4044686
-#     },
-#     {
-#         'title': 'work',
-#         'id': 5,
-#         'owner_id':1,
-#         'longitude': 37.7860898,
-#         'latitude': -122.3942683
-#     }]
+spotkeys = [
+    {
+        'title': 'home',
+        'id': 1,
+        'owner_id':1,
+        'longitude': 37.7813967,
+        'latitude': -122.4044686
+    },
+    {
+        'title': 'home',
+        'id': 2,
+        'owner_id':2,
+        'longitude': 37.7813967,
+        'latitude': -122.4044686
+    },    
+    {
+        'title': 'home',
+        'id': 3,
+        'owner_id':3,
+        'longitude': 37.7813967,
+        'latitude': -122.4044686
+    },
+    {
+        'title': 'home',
+        'id': 4,
+        'owner_id':4,
+        'longitude': 37.7813967,
+        'latitude': -122.4044686
+    },
+    {
+        'title': 'work',
+        'id': 5,
+        'owner_id':1,
+        'longitude': 37.7860898,
+        'latitude': -122.3942683
+    }]
 
 @app.route('/')
 def hi():
