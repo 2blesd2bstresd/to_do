9749483c6ae77fd450ab9d7481c324c63bc98678
Maxwell Howard <maxh@spotkey.com>
2015-03-24 13:57:11 -0700

    fix

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def hi():
 def login():
 
     # c = get_conn_cursor()
-    print "REQUEST: ", dir(request)
+    print "AUTHORIZATION: ", dir(request.authorization)
     print "HEADERS: ", dir(request.headers)
     return 'try again'
 
