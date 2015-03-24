e2b6e13ef358aa077d730b2e4e79947940d45564
Maxwell Howard <maxh@spotkey.com>
2015-03-24 10:47:57 -0700

    fix

--- app.py
+++ app.py
@@ -110,7 +110,8 @@ def get_user(user_id):
     user['last_name'] = u.get('last_name', None)
     user['profile_url'] = u.get('profile_url', None)
     user['username'] = u.get('username', None)
-    user['spotkeys'] = get_spotkeys(user_id, c)
+    for sk in get_spotkeys(user_id, c):
+        user['spotkeys'].append(sk)
 
     # get the users spotkeys
     # user['spotkeys'] = get_spotkeys(user_id)
