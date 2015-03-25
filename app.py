0304a7d3b43dd2854593c8506c86c4ad319a56eb
Maxwell Howard <maxh@spotkey.com>
2015-03-25 12:48:27 -0700

    fix

--- app.py
+++ app.py
@@ -18,31 +18,8 @@ app = Flask(__name__)
 app.config.from_object('config')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yymrdbzqoowsqh:1bmpBpFOiKPLzweXcuX04FASwB@ec2-23-21-183-70.compute-1.amazonaws.com:5432/d7p0rp7lvl3e7b'
 db = SQLAlchemy(app)
-# from models import Test
+from models import User
 
-class User(db.Model):
-
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
-    profile_url = db.Column(db.String)
-    username = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False)
-    email = db.Column(db.String)
-
-    def __init__(self, username, password, first_name=None, last_name=None, email=None, profile_url=None):
-
-        self.username = username
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.profile_url = profile_url
-
-    def __repr__(self):
-        return '<User %r>' & self.username
 
 def get_conn_cursor():
 
--- models.py
+++ models.py
@@ -1,15 +1,25 @@
 from app import db
 
-class Test(db.Model):
-	
-	__tablename__ = "test"
-	
-	value = db.Column(db.String, primary_key=True)
-	
-	def __init__(self, value):
+class User(db.Model):
 
-		self.value = value
+    __tablename__ = "users"
 
-	def __repr__(self):
-		return '<value %r>' % (self.value)
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    profile_url = db.Column(db.String)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String)
 
+    def __init__(self, username, password, first_name=None, last_name=None, email=None, profile_url=None):
+
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.profile_url = profile_url
+
+    def __repr__(self):
+        return '<User %r>' & self.username
