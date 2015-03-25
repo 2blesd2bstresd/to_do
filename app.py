10bc85aca6f1a193097c01e77ebe294a4df99c7e
Maxwell Howard <maxh@spotkey.com>
2015-03-25 11:32:05 -0700

    fix

--- app.py
+++ app.py
@@ -95,36 +95,37 @@ def login():
     username = auth.get('username', None)
     password = auth.get('password', None)
     c.execute("SELECT id, username, first_name, last_name, profile_url FROM users WHERE username=\'{0}\' AND password=\'{1}\'".format(username, password))
-    u = c.fetchall()
-    print u
-    if u:
-        u = u[0]
-        user = {}
-        user['id'] = u.get('id', None)
-        user['first_name'] = u.get('first_name', None)
-        user['last_name'] = u.get('last_name', None)
-        user['profile_url'] = u.get('profile_url', None)
-        user['username'] = u.get('username', None)
-        spotkeys = []
-        for sk in get_spotkeys(user['id'], c):
-            spotkeys.append(sk)
-        user['spotkeys'] = spotkeys
-        # get the users spotkeys
-        # user['spotkeys'] = get_spotkeys(user_id)
+    # u = c.fetchall()
+    # print u
+    # if u:
+    #     u = u[0]
+    #     user = {}
+    #     user['id'] = u.get('id', None)
+    #     user['first_name'] = u.get('first_name', None)
+    #     user['last_name'] = u.get('last_name', None)
+    #     user['profile_url'] = u.get('profile_url', None)
+    #     user['username'] = u.get('username', None)
+    #     spotkeys = []
+    #     for sk in get_spotkeys(user['id'], c):
+    #         spotkeys.append(sk)
+    #     user['spotkeys'] = spotkeys
+    #     # get the users spotkeys
+    #     # user['spotkeys'] = get_spotkeys(user_id)
 
-        # get the users contacts
-        contacts = []
-        c.execute("SELECT contact_username , contact_id, profile_url FROM Contacts WHERE primary_id=%s" % user['id'])
-        for con in c.fetchall():
-            contact = {'username': con.get('contact_username', None),
-                       'id': con.get('contact_id', None),
-                       'profile_url': con.get('profile_url', None)}
-            contacts.append(contact)
-        user['contacts'] = contacts
+    #     # get the users contacts
+    #     contacts = []
+    #     c.execute("SELECT contact_username , contact_id, profile_url FROM Contacts WHERE primary_id=%s" % user['id'])
+    #     for con in c.fetchall():
+    #         contact = {'username': con.get('contact_username', None),
+    #                    'id': con.get('contact_id', None),
+    #                    'profile_url': con.get('profile_url', None)}
+    #         contacts.append(contact)
+    #     user['contacts'] = contacts
         
-        return jsonify(user)
-    else:
-        return abort(401)
+    #     return jsonify(user)
+    # else:
+    #     return abort(401)
+    return 'GRAVY'
 
 
 @app.route('/add_user', methods=['POST'])
