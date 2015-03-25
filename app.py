d51017e10e2f6a41735d484cf05ab102826ab277
Maxwell Howard <maxh@spotkey.com>
2015-03-25 11:56:08 -0700

    fix

--- app.py
+++ app.py
@@ -96,8 +96,6 @@ def login():
     password = auth.get('password', None)
     # c.execute("SELECT id, username, first_name, last_name, profile_url FROM users WHERE username=\'{0}\' AND password=\'{1}\'".format(username, password))
     u = db.session.query(User).filter_by(username=username).filter_by(password=password)
-
-    u = c.fetchall()
     print u
     if u:
         u = u[0]
