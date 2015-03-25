91a0f0ee6e88237615cc6a19e8a5d201f78e6b46
Maxwell Howard <maxh@spotkey.com>
2015-03-24 18:08:17 -0700

    fix

--- models.py
+++ models.py
@@ -1,16 +1,15 @@
 from app import db
-import datetime
 
 class Test(db.Model):
-
+	
 	__tablename__ = "test"
-
+	
 	value = db.Column(db.String, primary_key=True)
-
+	
 	def __init__(self, value):
 
-		self.value = name
+		self.value = value
 
 	def __repr__(self):
-		return '<name %r>' % (self.value)
+		return '<value %r>' % (self.value)
 
