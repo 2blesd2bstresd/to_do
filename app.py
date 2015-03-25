bc63d88f771e3b65c168f47c4ba6d062d14a4689
Maxwell Howard <maxh@spotkey.com>
2015-03-24 17:14:22 -0700

    fix

--- app.py
+++ app.py
@@ -61,6 +61,12 @@ def get_spotkeys(user_id, c):
 def hi():
     return 'vielkom and bienvenue.'
 
+@app.route('/test/<string:value>', methods =['GET']):
+def test(value):
+    test = Test(value)
+    db.session.add(value)
+    db.session.commit()
+
 @app.route('/login', methods=['POST'])
 def login():
 
