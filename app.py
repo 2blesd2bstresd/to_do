46ab1ac3abf71d9fe51f3c7a87f2c3369c12a78c
Maxwell Howard <maxh@spotkey.com>
2015-03-25 18:08:20 -0700

    fix

--- app.py
+++ app.py
@@ -236,13 +236,12 @@ def create_spotkey():
                  city, state, zipcode, buzzer_code, door_number, 
                  details, cross_street)
 
-    # db.session.add(s)
-    # db.session.commit()
+    db.session.add(s)
+    db.session.commit()
 
-    # sk.primary_spot_id = s.id
-    # db.session.add(sk)
-    # db.session.commit()
-    #
+    sk.primary_spot_id = s.id
+    db.session.add(sk)
+    db.session.commit()
 
     return jsonify({'status_code':200, 'date':datetime.now(), 'spotkey_id': sk.id})
 
