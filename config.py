83d4adcabf01d80bd65dff1ba0e45e6cd45c6d34
Maxwell Howard <maxh@spotkey.com>
2015-04-01 12:21:26 -0700

    fix

--- config.py
+++ config.py
@@ -3,15 +3,15 @@ import os
 # grabs the folder where the script runs
 basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'd7p0rp7lvl3e7b' 
-USERNAME = 'yymrdbzqoowsqh' 
-PASSWORD = '1bmpBpFOiKPLzweXcuX04FASwB' 
+USERNAME = 'ngdndiaovlpgps' 
+PASSWORD = 'yBzzZ1UBU7c2phcIMBfB4HHA5u' 
 CSRF_ENABLED = True 
 # SECRET_KEY = 'my_precious'
 # defines the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
 
 # DB URL
-URL = '//yymrdbzqoowsqh:1bmpBpFOiKPLzweXcuX04FASwB@ec2-23-21-183-70.compute-1.amazonaws.com:5432/d7p0rp7lvl3e7b'
+URL = '//ngdndiaovlpgps:yBzzZ1UBU7c2phcIMBfB4HHA5u@ec2-23-21-183-70.compute-1.amazonaws.com:5432/d7p0rp7lvl3e7b'
 
 # database URI
-SQLALCHEMY_DATABASE_URI = 'postgres://yymrdbzqoowsqh:1bmpBpFOiKPLzweXcuX04FASwB@ec2-23-21-183-70.compute-1.amazonaws.com:5432/d7p0rp7lvl3e7b'
+SQLALCHEMY_DATABASE_URI = 'postgres://ngdndiaovlpgps:yBzzZ1UBU7c2phcIMBfB4HHA5u@ec2-23-21-183-70.compute-1.amazonaws.com:5432/d7p0rp7lvl3e7b'
