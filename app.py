0218abe00d413e3184ee75fbf694b8ad269c12af
Maxwell Howard <maxh@spotkey.com>
2015-04-17 10:28:01 -0700

    update

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def get_spotkeys(user_id=None, spotkey_ids=None):
     """
 
     if user_id:
-        spotkeys = Spotkey.query.filter_by(owner_id=user_id)
+        spotkeys = Spotkey.query.filter_by(owner_id=user_id) \
                                 .filter_by(share_with_all=True) 
     else:
         spotkeys = [Spotkey.query.filter_by(id=sk_id) \
