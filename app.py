931f65cf9a83174ef1c18da6c84aa6f5ee724b52
Maxwell Howard <maxh@spotkey.com>
2015-04-16 17:31:48 -0700

    update

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def get_spotkeys(user_id=None, spotkey_ids=None):
                                  .filter_by(share_with_all=True) \
                                  .first() for sk_id in spotkey_ids]
 
-    print "USER ID: ", user_id
+    print "USER ID: ", type(user_id)
     print "SPOCKIS: ", spotkeys.all()
 
     sk_list = []
