f878624cf518cba56351480de6f393bee59e34ef
Maxwell Howard <maxh@spotkey.com>
2015-04-21 16:53:54 -0700

    update

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def get_spotkeys(user_id=None, spotkey_ids=None, tether=False):
                         'owner_id' : sk.owner_id,
                         'primary_spot_id': sk.primary_spot_id,
                         'owner_profile_url': sk.profile_url,
-                        'nav_types': sk.nav_types.split()
+                        'nav_types': sk.nav_types.split(':')
                        }
             sk_list.append(spotkey)
 
